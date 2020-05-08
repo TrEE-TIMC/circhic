f037075e9f1a000ea43c9e14aea41c6945dd1eb4
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-05-08 11:12:14 +0200

    DOC API doc + numpydoc
    Activating the API build

--- circhic/datasets/_base.py
+++ circhic/datasets/_base.py
@@ -7,6 +7,14 @@ def load_ccrescentus():
     """
     Loads *C. crescentus* contact counts.
 
+    Returns
+    -------
+
+    counts, lengths :
+        a tuple containing an (n, n) ndarray corresponding to the raw contact
+        counts for *C. crescentus*, and an (l, ) ndarray containing the
+        lengths of all chromosomes.
+
     """
     module_path = os.path.dirname(__file__)
     counts = io.load_counts(
---
+++ doc/modules/classes.rst
@@ -0,0 +1,20 @@
+:orphan:
+
+.. _api_ref:
+
+=============
+API Reference
+=============
+
+
+Datasets
+========
+
+.. currentmodule:: circhic
+
+.. autosummary::
+
+   :toctree: generated/
+   :template: function.rst
+
+   datasets.load_ccrescentus
