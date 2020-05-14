e200e674bcaaf60ceaf4299c352cd000c211c1ce
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-05-14 11:24:17 +0200

    ENH new version of tools.py

--- circhic/tools.py
+++ circhic/tools.py
@@ -1,15 +1,15 @@
 import sys
 import numpy as np
 from scipy import sparse
-# from IPython.core.display import clear_output
+from IPython.core.display import clear_output
 
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-#def display(disp = ''):
-#    clear_output();
-#    print(disp);
-#    sys.stdout.flush()
+def display(disp = ''):
+    clear_output();
+    print(disp);
+    sys.stdout.flush()
     
 
 def load_HiC(filename, n_bins):
@@ -42,7 +42,7 @@ def load_HiC(filename, n_bins):
 
     return counts
 
-def genCircData(HiC,bin_circ=0.5,r_in=0.5,res=0,s_in=800000,s_out=800000,pos0=1):
+def genCircData(HiC,bin_circ=0.5,r_in=0.5,res=0,s_in=800000,s_out=800000,pos0=1,mode='circ',frac_lin=0.7,rotate_lin=0):
     """Generate Circular Strip Data
     
     
@@ -69,6 +69,17 @@ def genCircData(HiC,bin_circ=0.5,r_in=0.5,res=0,s_in=800000,s_out=800000,pos0=1)
     pos0: integer
         Genomic position at the vertical top        
         
+    mode : string
+        'circ': circular chromosome, default
+        'lin': linear chromosome
+        
+    frac_lin : float <= 1
+        fraction of strip filled by linear chromosome
+        
+    rotate_lin : float <= 360
+        rotation angle (in degree) of the chromosome
+        (the linear chromosome is symmetric with respect to the vertical axis by default)
+                    
     Returns
     -------
     (Nc, Nc) ndarray containing the count data projected on a circular strip.
@@ -82,35 +93,43 @@ def genCircData(HiC,bin_circ=0.5,r_in=0.5,res=0,s_in=800000,s_out=800000,pos0=1)
     
     N,Nc=int(len(HiC)),int(len(HiC)/bin_circ)
     
-    #converting indexes of the output matrix into real values (from 0 to 2) such that the size of the matrix is 2 and, hence, the outer radius of the strip is 1
-    X= np.tile(2*np.arange(Nc)/Nc-1,(Nc,1))    
-
+    #converting indexes of the output matrix into real values (from -1 to 1) such that the size of the matrix is 2 and, hence, the outer radius of the strip is 1
+    V= np.tile(2*np.arange(Nc)/Nc-1,(Nc,1))
+    
     #corresponding radii (R)
-    X2=X*X
-    R = np.sqrt(X2+np.transpose(X2))
+    V2=V*V
+    R = np.sqrt(V2+np.transpose(V2))
         
     # indexes of non-zero R entries (to work only with the circular strip entries)
     iR = (R >= r_in) & (R <= 1)
         
-    #corresponding polar angles (Theta)
-    ThetaX, ThetaY = np.zeros((Nc,Nc)), np.zeros((Nc,Nc))
-    ThetaX[iR] = np.arccos(X[iR]/R[iR])
-    ThetaY[iR] = np.arcsin(np.transpose(X)[iR]/R[iR])
+    #corresponding CLOCKWISE polar angles (Theta); here, we need to consider separately the angles given by acos and asin
+    #REM: genomic coordinate goes clockwise
+    Theta_cos, Theta_sin= np.zeros((Nc,Nc)), np.zeros((Nc,Nc))    
+    Theta_cos[iR] = np.arccos(V[iR]/R[iR])
+    Theta_sin[iR] = np.arcsin(np.flip(np.transpose(V)[iR]/R[iR]))
     
-    Theta = np.array(ThetaX)
+    #default: the CLOCKWISE angle is given by the acos when the asin is negative
+    Theta = np.array(Theta_cos)
+   
+    #if asin is positive, the angle depends on the value of acos 
+    iT = (Theta_sin>=0) & (Theta_cos>np.pi/2)
+    Theta[iT & iR] = np.pi+Theta_sin[iT & iR]
     
-    iT = (ThetaY<=0) & (ThetaX>np.pi/2)
-    Theta[iT & iR] = ThetaX[iT & iR] - 2*ThetaY[iT & iR]
-    
-    iT = (ThetaY<=0) & (ThetaX<=np.pi/2)
-    Theta[iT & iR] = 2*np.pi + ThetaY[iT & iR]
-        
-    shift_theta=(pos0-1)/Lg*2*np.pi    
-    Theta[iR] = (Theta[iR] + np.pi/2+shift_theta)%(2*np.pi)            
+    iT = (Theta_sin>=0) & (Theta_cos<=np.pi/2)
+    Theta[iT & iR] = 2*np.pi - Theta_sin[iT & iR]
+
+    #setting the origin of the clockwise polar angle at the top vertical
+    if mode=='circ': Theta[iR] = (Theta[iR] + np.pi/2)%(2*np.pi)
+    elif mode=='lin': Theta[iR] = (Theta[iR] + np.pi/2+frac_lin*np.pi-rotate_lin/360*2*np.pi)%(2*np.pi)
+    else: sys.exit('Unknown mode for generating circular data')
 
     #Bref is the bin "x" of the ms where we consider a contact between x-s/2 and x+s/2
     Bref = np.zeros((Nc,Nc),dtype=int)
-    Bref[iR] = np.minimum((Theta[iR]/2/np.pi*N).astype(int),N-1) #should work without the minimum... (just a precaution)
+    if mode=='circ': Bref[iR] = ((((pos0-1)/Lg+Theta[iR]/2/np.pi)*N).astype(int))%N
+    else:
+        #with this defition, angles larger than frac_lin*2*pi are irrelevant, which is handled at the very end
+        Bref[iR] = (((Theta[iR]/2/np.pi/frac_lin)*N).astype(int))%N 
 
     #Half_s is the corresponding "s/2" in x-s/2 and x+s/2
     Half_s = np.zeros((Nc,Nc),dtype=int)
@@ -123,11 +142,18 @@ def genCircData(HiC,bin_circ=0.5,r_in=0.5,res=0,s_in=800000,s_out=800000,pos0=1)
     
     #filling up the final matrix
     C = np.empty((Nc,Nc)) * np.nan #background mask
-    for ic,jc in np.argwhere(iR>0): C[ic,jc] = HiC[Ih[ic,jc],Jh[ic,jc]]
+    
+    if mode=='circ':
+        for ic,jc in np.argwhere(iR>0): C[ic,jc] = HiC[Ih[ic,jc],Jh[ic,jc]]
+    else:
+        #handling angles larger than frac_lin*2*pi
+        #handling non-periodic conditions
+        for ic,jc in np.argwhere((iR>0) & (Theta<=frac_lin*2*np.pi) & (Bref - Half_s > 0) & (Bref + Half_s < N)):
+            C[ic,jc] = HiC[Ih[ic,jc],Jh[ic,jc]]
     
     return C
 
-def plotCirc_simple(Circ,H):
+def plotCirc_simple(Circ,H,colorbar=True):
     """Plot a circular strip
         
     Parameters
@@ -138,6 +164,9 @@ def plotCirc_simple(Circ,H):
     H : ndarray
         corresponding original HiC matrix, to compute vmax and vmin
              
+    colorbar : boolean
+        if true, plot colorbar
+        
     Returns
     -------
     An image can be used with e.g. plt.subplot
@@ -150,10 +179,11 @@ def plotCirc_simple(Circ,H):
     plt.imshow(Circ,norm=colors.SymLogNorm(min_non_zero),vmin=vmin,vmax=vmax,cmap='viridis')
     plt.xticks([]); plt.yticks([]);
     plt.axis('off')
+    if colorbar: plt.colorbar(shrink=0.7);
     
     return
 
-def plotCirc(H,r_in,s_in,s_out,genomic_data,marks={},bin_circ=0.5,res=10000,pos0=1):
+def plotCirc(H,r_in,s_in,s_out,genomic_data,marks={},bin_circ=0.5,res=10000,pos0=1,colorbar=True):
     """Plot a circular strip with the option to add genomic data and marks
         
     The plot requires the original HiC matrix (to generate the circular strip matrix) because it makes radii handling more robust 
@@ -191,7 +221,10 @@ def plotCirc(H,r_in,s_in,s_out,genomic_data,marks={},bin_circ=0.5,res=10000,pos0
         
     pos0: integer
         Genomic position at the vertical top        
-             
+           
+    colorbar : boolean
+        if true, plot colorbar
+                              
     Returns
     -------
     An image that cannot be used with e.g. plt.subplot
@@ -217,10 +250,11 @@ def plotCirc(H,r_in,s_in,s_out,genomic_data,marks={},bin_circ=0.5,res=10000,pos0
     #circular strip
     axis_c=np.array([np.max(((1-1/rmax_g)/2,0)), np.max(((1-1/rmax_g)/2,0)), np.min((1/rmax_g,1)), np.min((1/rmax_g,1))])
     ax_c=fig.add_axes(axis_c)
-    ax_c.imshow(Circ,norm=colors.SymLogNorm(min_non_zero),vmin=vmin,vmax=vmax,cmap='viridis')
+    img=ax_c.imshow(Circ,norm=colors.SymLogNorm(min_non_zero),vmin=vmin,vmax=vmax,cmap='viridis')
+    if colorbar: plt.colorbar(img, ax=ax_c, shrink=0.7)
     plt.xticks([]); plt.yticks([]);
     plt.axis('off')
-
+            
     #genomic data
     if 'data' in genomic_data:
         axis_g=np.array([np.max(((1-rmax_g)/2,0)), np.max(((1-rmax_g)/2,0)), np.min((rmax_g,1)), np.min((rmax_g,1))])
