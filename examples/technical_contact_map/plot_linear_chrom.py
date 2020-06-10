540ac793b2103e505e7d46ab97ae77caee129587
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-06-10 10:25:16 +0200

    ENH adding support for linear chromosomes

---
+++ examples/technical_contact_map/plot_linear_chrom.py
@@ -0,0 +1,15 @@
+"""
+===========================
+Plotting linear chromosomes
+===========================
+"""
+
+from circhic import datasets, CircHiCFigure
+
+data = datasets.load_kbm7()
+counts = data["counts"]
+nbins = data["nbins"]
+
+
+circfig = CircHiCFigure(lengths=nbins, chromosome_type="linear")
+circfig.plot_hic(counts)
