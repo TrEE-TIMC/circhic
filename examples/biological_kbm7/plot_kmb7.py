7da441da5beeeaf52990d39ef7678c8fa5c2656a
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-07-20 14:24:43 +0200

    DOC adding labels / genomic information for KBM7

--- examples/biological_kbm7/plot_kmb7.py
+++ examples/biological_kbm7/plot_kmb7.py
@@ -15,12 +15,19 @@ lengths = data["nbins"]
 
 cum_raw_counts = counts.sum(axis=1)
 
-circhicfig = CircHiCFigure(lengths, chromosome_type="linear")
-circhicfig.plot_hic(counts, inner_radius=0.5, outer_radius=0.89, cmap="bone_r",
-                    inner_gdis=80, outer_gdis=80)
-_, ax = circhicfig.plot_bars(
+circfig = CircHiCFigure(lengths, chromosome_type="linear")
+circfig.plot_hic(counts, inner_radius=0.5, outer_radius=0.89, cmap="bone_r",
+                 inner_gdis=80, outer_gdis=80)
+_, ax = circfig.plot_bars(
     cum_raw_counts,
     inner_radius=0.9,
     outer_radius=0.99,
     color="0")
-circhicfig.set_genomic_ticklabels()
+circfig.set_genomic_ticklabels(
+    fontdict={"fontsize": "small", "color": "0.3"})
+
+rax = circfig.plot_raxis()
+rax.set_yticklabels(["6", "0", "6"], fontsize="small")
+
+rax.set_ylabel("Genomic distance (Mb)", fontsize="small", color="0.3",
+               position=(0, 1.03))
